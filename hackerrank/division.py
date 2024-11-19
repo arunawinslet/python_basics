bb465357e3fd1b438d868b8a70c9985f69dfc11d
arunawinslet <arunawinslet.aw@gmail.com>
2024-11-19 18:35:08 +0530

    simple division

---
+++ hackerrank/division.py
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/python-division/submissions/code/41056589
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
